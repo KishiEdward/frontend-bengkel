8bf4f9c2de844c2d7173443e79f924b43be18366
KishiEdward <dzidanrh@gmail.com>
2026-05-23 13:35:44 +0700

    feat: add new navigator to detail_pesanan

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'ui/screens/login_screen.dart';
 import 'providers/pesanan_provider.dart';
+import 'providers/detail_pesanan_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => PesananProvider()),
+        ChangeNotifierProvider(create: (_) => DetailPesananProvider()),
       ],
       child: MaterialApp(
         title: 'Bengkel Bubut ABC',
