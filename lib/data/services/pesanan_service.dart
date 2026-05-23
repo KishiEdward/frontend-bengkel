83cff113546a5a471d5b11861c4cca4f2989a5fa
KishiEdward <dzidanrh@gmail.com>
2026-05-23 12:52:31 +0700

    feat: add pesanan_service

---
+++ lib/data/services/pesanan_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../core/constants.dart';
+import '../models/pesanan_model.dart';
+
+class PesananService {
+  Future<List<Pesanan>> getSemuaPesanan() async {
+    // 1. Ambil token dari brankas lokal HP
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('jwt_token');
+
+    if (token == null) {
+      throw Exception("Sesi telah habis, silakan login kembali.");
+    }
+
+    // 2. Tembak API Golang dengan membawa Token
+    final response = await http.get(
+      Uri.parse('${AppConstants.baseUrl}/pesanan'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token', // <-- INI KUNCI PINTUNYA!
+      },
+    );
+
+    // 3. Proses Datanya
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      final List<dynamic> dataList = responseData['data'];
+      
+      // Ubah list JSON menjadi list Objek Pesanan
+      return dataList.map((json) => Pesanan.fromJson(json)).toList();
+    } else {
+      throw Exception("Gagal mengambil data pesanan");
+    }
+  }
+}
